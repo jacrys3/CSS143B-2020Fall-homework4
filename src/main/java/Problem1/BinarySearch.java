c9bd45305633b262b32cd671ead7ed74eb1b491d
Jacrys3 <ryse0528@gmail.com>
2020-11-01 12:44:35 -0800

    First attempt at BinarySearch

--- src/main/java/Problem1/BinarySearch.java
+++ src/main/java/Problem1/BinarySearch.java
@@ -5,7 +5,22 @@ public class BinarySearch {
     // Do not change signature (return type, function name, parameter types)
     // Add a helper function for recursion calls
     public static int binarySearch(int[] data, int target) {
-        // homework
-        return -1;  // place holder
+        return helperSearch(data, target, 0, data.length);
+    }
+
+    private static int helperSearch(int[] data, int target, int start, int end){
+        if(data.length == 0 || (data.length == 1 && data[0] != target)) return -1;
+        else if(data.length == 1 && data[0] == target) return 0;
+
+        int mid = (end - start) / 2;
+        if(data[mid] < target){
+            end = mid;
+            return helperSearch(data, target, start, end);
+        } else if(data[mid] > target){
+            start = mid;
+            return helperSearch(data, target, start, end);
+        } else{
+            return mid;
+        }
     }
 }
