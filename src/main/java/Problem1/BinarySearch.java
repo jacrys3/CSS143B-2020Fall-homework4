baa3b33a69346519e7c4ea5eacc78563fe97131b
Jacrys3 <ryse0528@gmail.com>
2020-11-01 12:58:06 -0800

    reviewed my hw 1 binarySearch and found out how i was doing this one wrong, passes test now

--- src/main/java/Problem1/BinarySearch.java
+++ src/main/java/Problem1/BinarySearch.java
@@ -1,23 +1,22 @@
 package Problem1;
-//start
+
 public class BinarySearch {
     // This is to be done recursively
     // Do not change signature (return type, function name, parameter types)
     // Add a helper function for recursion calls
     public static int binarySearch(int[] data, int target) {
+        if(data.length == 0 || (data.length == 1 && data[0] != target)) return -1;
+        else if(data.length == 1 && data[0] == target) return 0;
         return helperSearch(data, target, 0, data.length);
     }
 
     private static int helperSearch(int[] data, int target, int start, int end){
-        if(data.length == 0 || (data.length == 1 && data[0] != target)) return -1;
-        else if(data.length == 1 && data[0] == target) return 0;
-
-        int mid = (end - start) / 2;
+        int mid = (end + start) / 2;
         if(data[mid] < target){
-            end = mid;
+            start = mid + 1;
             return helperSearch(data, target, start, end);
         } else if(data[mid] > target){
-            start = mid;
+            end = mid - 1;
             return helperSearch(data, target, start, end);
         } else{
             return mid;
