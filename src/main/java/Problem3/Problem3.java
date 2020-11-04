4e43f8ce65a88fdabec463e0ca44031bd0786c85
Jacrys3 <ryse0528@gmail.com>
2020-11-04 15:48:11 -0800

    problem 3 done? seems too easy

--- src/main/java/Problem3/Problem3.java
+++ src/main/java/Problem3/Problem3.java
@@ -5,6 +5,7 @@ import Problem2.ListNode;
 import java.io.PrintStream;
 
 public class Problem3 {
+        // got help from geeks for geeks.com
     public static void printListInReverse(ListNode list, PrintStream print) {
         if(list != null) {
             if(list.next != null)
